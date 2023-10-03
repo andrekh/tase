c5aa304e8ec5bcbfdbd89b096d7aa631c2c3fe97
smallguy <smallguy@pop-os.localdomain>
2023-10-02 21:42:24 -0400

    added mohaahandler to json

--- src/mohaahandler.cpp
+++ src/mohaahandler.cpp
@@ -1,5 +1,6 @@
 #include "mohaahandler.h"
-
+#include <string>
+#include <map>
 
 //===============================================================================
 MohaaHandler::MohaaHandler(const std::string& ServerIP)
@@ -23,11 +24,47 @@ void MohaaHandler::AskServerForInfo()
         //error
         }
 
-    int a = 0;
+    ParseInfoResponse();
     }
 
 //===============================================================================
 std::string MohaaHandler::GetQueryPort(const std::string &Port)
     {
     return std::to_string(std::stoi(Port) + 97);
+    }
+
+//===============================================================================
+void MohaaHandler::ParseInfoResponse()
+    {
+    std::map<std::string, std::string> ResponseMap;
+    std::string Response(&m_ServerResponse[1]);
+    std::string Key {};
+    std::string Value {};
+
+    do {
+        std::size_t Pos = Response.find("\\");
+        Key = Response.substr(0, Pos);
+        Response = Response.substr(Pos + 1);
+        Pos = Response.find("\\");
+        if(Pos != std::string::npos)
+            {
+            Value = Response.substr(0, Pos);
+            Response = Response.substr(Pos + 1);
+            }
+        else
+            {
+            Value = Response;
+            Response = {};
+            }
+
+        ResponseMap[Key] = Value;
+        } while(!Response.empty());
+
+    // at throws exception if key not found
+    m_ServerName = ResponseMap.at("hostname");
+    m_Map = ResponseMap.at("mapname");
+    m_GameType = ResponseMap.at("gametype");
+    m_MaxPlayers = (uint8_t) std::stoi(ResponseMap.at("maxplayers"));
+    m_PlayersCount =  (uint8_t) std::stoi(ResponseMap.at("numplayers"));
+    m_QuerySuccess = true;
     }
--- src/mohaahandler.h
+++ src/mohaahandler.h
@@ -3,15 +3,36 @@
 
 #include "serverhandler.h"
 #include <string>
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 class MohaaHandler : public ServerHandler
     {
     public:
         MohaaHandler(const std::string& ServerIP);
         virtual void AskServerForInfo() override;
+        std::string GetGameType() const { return m_GameType; }
 
     protected:
         virtual std::string GetQueryPort(const std::string &Port) override;
+        void ParseInfoResponse();
+
+    private:
+        std::string m_GameType {};
     };
+
+void to_json(json& j, const MohaaHandler& Mh)
+    {
+        j[Mh.GetServerIP()] =
+            {
+                {"status", Mh.QuerySuccess()},
+                {"name", Mh.GetServerName()},
+                {"map", Mh.GetCurrentMap()},
+                {"gametype", Mh.GetGameType()},
+                {"players", Mh.GetPlayersCount()},
+                {"max players", Mh.GetMaxPlayers()},
+            };
+    }
 
 #endif
